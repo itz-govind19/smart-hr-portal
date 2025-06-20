6ff6364a960e12fed2824f141104ecd6cb3ba22d
itz_govind <govind.sirsath@adoptnet.tech>
2025-06-20 15:07:43 +0530

    changed Department table architecture

--- departmentservice/departmentservice/src/main/java/com/demo/departmentservice/entity/Department.java
+++ departmentservice/departmentservice/src/main/java/com/demo/departmentservice/entity/Department.java
@@ -1,18 +1,16 @@
-package com.demo.employeeservice.entity;
+package com.demo.departmentservice.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Department {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
